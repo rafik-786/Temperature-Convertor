415d5efb117144fbb3a37832a1ae5f8ad81570b7
Rafikul Alam <rafikulalam2000off@gmail.com>
2022-10-24 20:18:05 +0530

    Chaged Structure

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let inputCel=document.getElementById('input-cel');
 
 inputCel.addEventListener("input",function(){
     
+
     let c = this.value ;
+
+    inputfahr.style.boxShadow="-5px 0px 25px rgba(95, 217, 225, 0.895)";
+    inputCel.style.boxShadow="";
 
     let f = c*1.8+32;
 
@@ -12,11 +16,15 @@ inputCel.addEventListener("input",function(){
 
     inputfahr.value=f;
 
+
+
 });
 inputfahr.addEventListener("input",function(){
     
     let f = this.value;
 
+    inputCel.style.boxShadow="-5px 0px 25px rgba(95, 217, 225, 0.895)";
+    inputfahr.style.boxShadow="";
     let c = (f -32) *(5/9);
 
     if(!Number.isInteger(c)) c = c.toFixed(4);
